37f65a511046b583b6b3f21baaf52e94c1f3c4b8
jugglingcats <github@akirkpatrick.com>
2022-05-27 09:27:25 +0100

    Initial commit

--- react-ace-module/index.js
+++ react-ace-module/index.js
@@ -2,6 +2,8 @@ import * as React from "react"
 import ReactAce from "react-ace";
 
 export const MyReactAce=() => {
+    // switch the two lines below to get the app working!
+
     return React.createElement(ReactAce, {}, null)
     // return React.createElement(ReactAce.default, {}, null)
 }
